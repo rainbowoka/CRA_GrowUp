3a840ebc74b2ff57c92596f08133a69e1f33f3fc
losic13 <losic13@naver.com>
2022-03-18 11:34:01 +0900

    feature : Employee Class Create

---
+++ EmployeeManager/src/main/java/Employee.java
@@ -0,0 +1,17 @@
+class Employee {
+    public String employeeNum;
+    public String name;
+    public String cl;
+    public String phoneNum;
+    public String birthday;
+    public String certi;
+
+    public Employee(String employeeNum, String name, String cl, String phoneNum, String birthday, String certi) {
+        this.employeeNum = employeeNum;
+        this.name = name;
+        this.cl = cl;
+        this.phoneNum = phoneNum;
+        this.birthday = birthday;
+        this.certi = certi;
+    }
+}
