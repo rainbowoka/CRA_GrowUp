cf5c3eea61749fd5565fd5bc8ade64374299d33f
losic13 <losic13@naver.com>
2022-03-21 11:49:15 +0900

    # feature : HashMap index 정렬하는 기능 완성
    # test: EmployeeManager에 2000여개 데이터 삽입 샘플 추가

--- EmployeeManager/src/main/java/EmployeeManager.java
+++ EmployeeManager/src/main/java/EmployeeManager.java
@@ -3,7 +3,9 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class EmployeeManager {
-    private String[] indexList = new String[] {"employeeNum", "name", "name_first", "name_last", "cl", "phoneNum", "phoneNum_middle","phoneNum_last", "birthday", "birth_yy", "birth_mm", "birth_dd", "certi"};
+    public static final String EMPTY = " ";
+
+    private String[] indexList = new String[] {"employeeNum", "name", "name_first", "name_last", "cl", "phoneNum", "phoneNum_middle","phoneNum_last", "birthday", "birthday_yy", "birthday_mm", "birthday_dd", "certi"};
     private HashMap<String, ArrayList<Employee>> index = new HashMap<>();
 
     public EmployeeManager()
@@ -13,12 +15,7 @@ public class EmployeeManager {
         }
     }
 
-    public ArrayList<Employee> getDataByKey(String indexName)
-    {
-        return index.get(indexName);
-    }
-
-    public void Add(ArrayList<Employee> emps)
+    public void add(ArrayList<Employee> emps)
     {
         for(Employee e:emps) {
             if(isValid(e) == false){
@@ -27,44 +24,112 @@ public class EmployeeManager {
         }
         for(String key : indexList) {
             index.get(key).addAll(emps);
-            RestructIndex(key);
         }
+
+        restructIndex();
     }
 
     protected boolean isValid(Employee e) {
         if (isValidEmpNo(e.employeeNum) == false)
             return false;
+        if(isValidName(e.name) == false)
+            return false;
+
         return true;
     }
     protected boolean isValidEmpNo(String empNo) {
+        if(index.get("employeeNum").stream().filter(x->x.employeeNum == empNo).count() != 0)
+            return false;
+
         if (empNo.matches("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]") == true)
             return true;
         else
             return false;
     }
-
-
-
-
-    private void RestructIndex(String key)
-    {
-        Collections.sort(index.get(key), (t1, t2) -> {
-            if (t1.name.compareTo(t2.name) < 0)
-                return 1;
-            else
-                return -1;
-        });
+    protected boolean isValidName(String name) {
+        //중간에 띄워쓰기가 있는 한글,영문
+        if (name==EMPTY || name.matches("[aA-zZ가-힣]+( )[aA-zZ가-힣]+") == true)
+            return true;
+        else
+            return false;
     }
 
-    public List<Employee> Search(Predicate<Employee> p)
+    private void restructIndex()
     {
-        ArrayList<Employee> empNoindex = index.get("employeeNum");
+        sortEmployeeNum();
+        sortName();
+        sortCareerLevel();
+        sortPhoneNum();
+        sortBirthday();
+        sortCerti();
+    }
+    private void sortObject(String indexKey){
+        Collections.sort(index.get(indexKey), (t1, t2) -> compareTo(t1.GetObject(indexKey), t2.GetObject(indexKey), t1.GetObject("employeeNum"), t2.GetObject("employeeNum")));
+    }
+
+    private void sortEmployeeNum()
+    {
+        sortObject("employeeNum");
+    }
+    private void sortName(){
+        sortObject("name");
+        sortObject("name_first");
+        sortObject("name_last");
+    }
+
+    private void sortCareerLevel(){
+        sortObject("cl");
+    }
+
+    private void sortPhoneNum(){
+        sortObject("phoneNum");
+        sortObject("phoneNum_middle");
+        sortObject("phoneNum_last");
+    }
+    private void sortBirthday(){
+        sortObject("birthday");
+        sortObject("birthday_yy");
+        sortObject("birthday_mm");
+        sortObject("birthday_dd");
+    }
+    private void sortCerti(){
+        sortObject("certi");
+    }
+
+    private int compareTo(String compare1, String compare2){
+        if (compare1.compareTo(compare2) > 0) {
+            return 1;
+        }
+        else if (compare1.compareTo(compare2) < 0) {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    private int compareTo(String compare1, String compare2, String default1, String default2){
+        if (compare1.compareTo(compare2) > 0) {
+            return 1;
+        }
+        else if (compare1.compareTo(compare2) < 0) {
+            return -1;
+        }
+        else
+        {
+            return compareTo(default1, default2);
+        }
+    }
+
+    public List<Employee> search(String key, Predicate<Employee> p){
+        ArrayList<Employee> empNoindex = index.get(key);
         List<Employee> a = empNoindex.stream().filter(p).collect(Collectors.toList());
 
         return a;
     }
 
-    public void Delete(Employee e)
+    public void delete(Employee e)
     {
         for(String key : indexList)
             index.get(key).remove(e);
