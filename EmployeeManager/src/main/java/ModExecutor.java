abc95aa3530f4cbe4ed1a742dfd9fe95ec67924f
rainbowoka <leoneisjh@naver.com>
2022-03-22 18:25:06 +0900

    # fix : ModExecutor 버그 수정

--- EmployeeManager/src/main/java/ModExecutor.java
+++ EmployeeManager/src/main/java/ModExecutor.java
@@ -7,7 +7,6 @@ class ModExecutor implements Executor{
         System.out.println("Execute MOD with options");
 
         ArrayList<String> printString = new ArrayList<>(1);
-        //SearchManager searchManager = new SearchManager();
 
         List<Employee> empList = null;
 
@@ -44,7 +43,18 @@ class ModExecutor implements Executor{
         }
 
         if (command.getFieldList().get(0).equals("birthday")) {
-            empList = em.search("birthday", e->e.birthday.equals(command.getFieldList().get(1)));
+            if (command.getOptionList().get(1).getOption().equals("y")) {
+                empList = em.search("birthday", e->e.birthday_yy.equals(command.getFieldList().get(1)));
+            }
+            else if (command.getOptionList().get(1).getOption().equals("m")) {
+                empList = em.search("birthday", e->e.birthday_mm.equals(command.getFieldList().get(1)));
+            }
+            else if (command.getOptionList().get(1).getOption().equals("d")) {
+                empList = em.search("birthday", e->e.birthday_dd.equals(command.getFieldList().get(1)));
+            }
+            else {
+                empList = em.search("birthday", e->e.birthday.equals(command.getFieldList().get(1)));
+            }
         }
 
         if (command.getFieldList().get(0).equals("certi")) {
@@ -55,27 +65,26 @@ class ModExecutor implements Executor{
             return null;
         }
 
-        em.mod(empList, command.getFieldList().get(2), command.getFieldList().get(3));
-
         if (empList.stream().count() == 0) {
             printString.add("MOD,NONE");
             return printString;
         }
 
         if (command.getOptionList().get(0).getOption().equals("p")) {
-
             for (int i = 0; i < 5 && i < empList.size(); i++) {
-                String temp = "MOD," + empList.get(0).GetObject("employeeNum") + "," +
-                                       empList.get(0).GetObject("name") + "," +
-                                       empList.get(0).GetObject("cl") + "," +
-                                       empList.get(0).GetObject("phoneNum") + "," +
-                                       empList.get(0).GetObject("birthday") + "," +
-                                       empList.get(0).GetObject("certi") + ",";
+                String temp = "MOD," + empList.get(i).GetObject("employeeNum") + "," +
+                                       empList.get(i).GetObject("name") + "," +
+                                       empList.get(i).GetObject("cl") + "," +
+                                       empList.get(i).GetObject("phoneNum") + "," +
+                                       empList.get(i).GetObject("birthday") + "," +
+                                       empList.get(i).GetObject("certi");
                 printString.add(temp);
             }
 
             return printString;
         }
+
+        em.mod(empList, command.getFieldList().get(2), command.getFieldList().get(3));
 
         printString.add("MOD," + empList.size());
         return printString;
