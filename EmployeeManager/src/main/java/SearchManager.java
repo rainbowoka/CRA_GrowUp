fb692b4c3accb9d3f474de1ca30d14d18c2293bb
JeongEunhyuk <ehj04@naver.com>
2022-03-22 19:19:00 +0900

    # fix : SearchManager -> EmployeeManager DataLoad방식 변경 기존 index 로 수집 후 option에 맞게 필터링하면서 정렬되어있는 데이터를 사용하는 문제 발생 -> index를 optino에 따라 변경한 후 수정된 index로 데이터를 로드하는 방식으로 변경

--- EmployeeManager/src/main/java/SearchManager.java
+++ EmployeeManager/src/main/java/SearchManager.java
@@ -12,46 +12,77 @@ public class SearchManager {
         return indexList.contains(index);
     }
 
-    private Predicate<Employee> convertIndexToPredicateWithOption(String index, String option, String value){
+    private String convertIndex(String index, String option){
         if(index.equals("name")){
             if(option.equals("f")){
-                return (e -> e.name_first.equals(value));
+                return "name_first";
             }
             else if(option.equals("l")){
-                return (e -> e.name_last.equals(value));
+                return "name_last";
             }
-            return (e -> e.name.equals(value));
         }
         if(index.equals("phoneNum")){
             if(option.equals("m")){
-                return (e -> e.phoneNum_middle.equals(value));
+                return "phoneNum_middle";
             }
             else if(option.equals("l")){
-                return (e -> e.phoneNum_last.equals(value));
+                return "phoneNum_last";
             }
         }
         if(index.equals("birthday")){
             if(option.equals("y")){
-                return (e -> e.birthday_yy.equals(value));
+                return "birthday_yy";
             }
             else if(option.equals("m")){
-                return (e -> e.birthday_mm.equals(value));
+                return "birthday_mm";
             }
             else if(option.equals("d")){
-                return (e -> e.birthday_dd.equals(value));
+                return "birthday_dd";
             }
+        }
+        return index;
+    }
+
+    private Predicate<Employee> convertIndexToPredicateWithOption(String index, String value){
+        if(index.equals("name_first")) {
+            return (e -> e.name_first.equals(value));
+        }
+        else if(index.equals("name_last")) {
+            return (e -> e.name_last.equals(value));
+        }
+        else if(index.equals("name")) {
+            return (e -> e.name.equals(value));
+        }
+        else if(index.equals("phoneNum_last")) {
+            return (e -> e.phoneNum_last.equals(value));
+        }
+        else if(index.equals("phoneNum_middle")) {
+            return (e -> e.phoneNum_middle.equals(value));
+        }
+        else if(index.equals("phoneNum")) {
+            return (e -> e.phoneNum.equals(value));
+        }
+        else if(index.equals("birthday_yy")){
+            return (e -> e.birthday_yy.equals(value));
+        }
+        else if(index.equals("birthday_mm")){
+            return (e -> e.birthday_mm.equals(value));
+        }
+        else if(index.equals("birthday_dd")){
+            return (e -> e.birthday_dd.equals(value));
+        }
+        else if(index.equals("birthday")){
             return (e -> e.birthday.equals(value));
         }
-        if(index.equals("employeeNum")) {
+        else if(index.equals("employeeNum")) {
             return (e -> e.employeeNum.equals(value));
         }
-        if(index.equals("cl")){
+        else if(index.equals("cl")){
             return (e -> e.cl.equals(value));
         }
-        if(index.equals("certi")){
+        else if(index.equals("certi")){
             return (e -> e.certi.equals(value));
         }
-        System.out.println("option : " + option);
         throw new RuntimeException();
     }
 
@@ -59,9 +90,10 @@ public class SearchManager {
         if(isExistIndex(index) == false)
             throw new RuntimeException();
 
-        List<Employee> employeeList = EmployeeManager.getEmployeeList(index);
+        index = convertIndex(index, option);
 
-        employeeList = employeeList.stream().filter(convertIndexToPredicateWithOption(index, option, value)).collect(Collectors.toList());
+        List<Employee> employeeList = EmployeeManager.getEmployeeList(index);
+        employeeList = employeeList.stream().filter(convertIndexToPredicateWithOption(index, value)).collect(Collectors.toList());
 
         return employeeList;
     }
